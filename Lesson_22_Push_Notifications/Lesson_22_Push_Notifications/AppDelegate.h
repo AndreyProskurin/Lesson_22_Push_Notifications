6600073df2a58874e995c0218a9d0974001e77ba
Andrey Proskurin <andrewproskurin@gmail.com>
2017-10-27 19:44:35 +0300

    Single view app has been added.

---
+++ Lesson_22_Push_Notifications/Lesson_22_Push_Notifications/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_22_Push_Notifications
+//
+//  Created by Andrey Proskurin on 27.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson_22_Push_Notifications/Lesson_22_Push_Notifications/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson_22_Push_Notifications
+//
+//  Created by Andrey Proskurin on 27.10.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
